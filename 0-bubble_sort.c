7af59b9982e01e94d3d4b541c1b3ba3420ca5770
qweku2000 <joeadofonyarko@gmail.com>
2023-10-23 06:21:14 -0700

    Bubble sort successfully implemented

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ *@array: An array of integers to be sorted with the bubble sorting method
+ *@size:The size of the array
+ *bubble_sort - This sorts the array of integers using the bubble method of sorting
+*/
+
+
+void bubble_sort(int *array, size_t size)
+{
+  int temp;
+  size_t i,j;
+  
+  for(i = 0; i < size - 1;i++)
+    {
+      for(j = 0; j < size - 1 - i;j++)
+	{
+	  if (array[j] > array[j+1])
+	    {
+	      /**
+	       *swap elements 
+	      */
+	      temp = array[j];
+	      array[j] = array[j+1];
+	      array[j + 1] = temp;
+	    }
+	}
+    }
+}
+		
