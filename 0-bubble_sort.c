68104b4c2fb66ff68008adcb34f2dd17b9cc6282
qweku2000 <joeadofonyarko@gmail.com>
2023-10-23 07:56:51 -0700

    Added a logic to prevent printing if already swapped

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,8 +11,8 @@ void bubble_sort(int *array, size_t size)
 {
   int temp;
   size_t i,j;
-  
-  for(i = 0; i < size - 1;i++)
+  int swapped;
+  for(i = 0;i < size - 1;i++)
     {
       for(j = 0; j < size - 1 - i;j++)
 	{
@@ -24,8 +24,16 @@ void bubble_sort(int *array, size_t size)
 	      temp = array[j];
 	      array[j] = array[j+1];
 	      array[j + 1] = temp;
+	      swapped = 1;
 	    }
-	  print_array(array,size);
+	  if (swapped)
+	    {
+	      print_array(array,size);
+	    }
+	  if (!swapped)
+	    {
+	      break;
+	    }
 	}
     }
 }
