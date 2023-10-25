ead37dd58bd6dc14eadb2b675e8e694eb6d032e0
qweku2000 <joeadofonyarko@gmail.com>
2023-10-25 02:57:20 -0700

    Advanced shell sort task

--- sort.h
+++ sort.h
@@ -26,3 +26,4 @@ listint_t *create_listint(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
