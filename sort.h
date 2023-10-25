16f6505e0e4b870d86e0ebc5e467872fa0569274
qweku2000 <joeadofonyarko@gmail.com>
2023-10-25 03:09:20 -0700

    Advanced counting sort task

--- sort.h
+++ sort.h
@@ -28,3 +28,4 @@ size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
