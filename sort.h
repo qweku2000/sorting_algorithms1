1ec7ebd13ef21b8c28abe98ebf31654aae2a0f47
qweku2000 <joeadofonyarko@gmail.com>
2023-10-25 03:00:21 -0700

    Advanced coktail sort task

--- sort.h
+++ sort.h
@@ -27,3 +27,4 @@ void selection_sort(int *array, size_t size);
 size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
